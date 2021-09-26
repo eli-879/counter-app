e5046fbb046c1529133a9e093dab5bf171579b28
Eric Li <ericxli2001@gmail.com>
2021-09-26 18:20:25 +1300

    tinkering with react

---
+++ src/components/counter.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class Counter extends React.Component {
+	state = {
+		count: 0,
+		imageURL: "https://picsum.photos/200",
+		tags: [],
+	};
+
+	renderTags() {
+		if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+		return (
+			<ul>
+				{this.state.tags.map((tag) => (
+					<li key={tag}>{tag}</li>
+				))}
+			</ul>
+		);
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<span className={this.getBadgeClasses()}>
+					{this.formatCount()}
+				</span>
+				<button className="btn btn-secondary btn-sm">Increment</button>
+				<div>
+					{this.state.tags.length === 0 && "Please create a new tag!"}
+					{this.renderTags()}
+				</div>
+			</React.Fragment>
+		);
+	}
+
+	getBadgeClasses() {
+		let classes = "bg m-2 bg-";
+		classes += this.state.count == 0 ? "warning" : "primary";
+		return classes;
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? "Zero" : count;
+	}
+}
+
+export default Counter;
